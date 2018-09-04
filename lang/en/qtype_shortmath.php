cd6e1a848ef1453d62810d20fee2c52f818f90e6
Hans Georg <georg@schaathun.net>
2018-09-04 23:28:14 +0200

    Changed plugin name.  All obvious required changes have been made.  Can be tested.

---
+++ lang/en/qtype_shortmath.php
@@ -0,0 +1,32 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package    qtype
+ * @subpackage shortmath
+ * @author     Sebastian S. Gundersen <sebastsg@stud.ntnu.no> and Hans Georg Schaathun <hasc@ntnu.no>
+ * @copyright  2018 NTNU
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Simple Mathematics';
+$string['pluginname_help'] = 'Short answer question type for mathematical expressions using MathQuill, resulting in LaTeX answers.';
+$string['pluginname_link'] = 'question/type/shortmath';
+$string['pluginnameadding'] = 'Adding a ShortMath question';
+$string['pluginnameediting'] = 'Editing a ShortMath question';
+$string['pluginnamesummary'] = 'Short answer question type for mathematical expressions using MathQuill, resulting in LaTeX answers.';
