a8c7383ef09bc7349a5fb18d0e72dd8699305628
André Storhaug <andr3.storhaug@gmail.com>
2018-10-07 17:02:42 +0200

    Update Visual math input library with latest changes

--- visualmathinput/pull.sh
+++ visualmathinput/pull.sh
@@ -2,4 +2,4 @@
 
 cp $HOME/git/kqm/visual-math-input/*.css visualmathinput/
 cp $HOME/git/kqm/visual-math-input/*.js amd/src/
-rsync -av $HOME/git/kqm/visual-math-input/font/ font/
+rsync -av $HOME/git/kqm/visual-math-input/fonts/ visualmathinput/font/
