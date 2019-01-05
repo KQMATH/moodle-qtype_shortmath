ce7c46522c232ff31725e1d59077726de4c022ce
André Storhaug <andr3.storhaug@gmail.com>
2019-01-05 23:43:32 +0100

    Add PHPUnit unit test

---
+++ tests/helper.php
@@ -0,0 +1,185 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Test helpers for the ShortMath question type.
+ *
+ * @package    qtype_shortmath
+ * @author     André Storhaug <andr3.storhaug@gmail.com>
+ * @copyright  2018 NTNU
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+defined('MOODLE_INTERNAL') || die();
+
+
+/**
+ * Test helper class for the ShortMath question type.
+ *
+ * @author     André Storhaug <andr3.storhaug@gmail.com>
+ * @copyright  2018 NTNU
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class qtype_shortmath_test_helper extends question_test_helper {
+    public function get_test_questions() {
+        return array(
+            'integration',
+            'integration_exact',
+            'escapedwildcards'
+        );
+    }
+
+    /**
+     * Makes a ShortMath question with correct ansewer 'frog', partially
+     * correct answer 'toad' and defaultmark 1. This question also has a
+     * '*' match anything answer.
+     * @return qtype_shortmath_question
+     */
+    public function make_shortmath_question_integration() {
+        question_bank::load_question_definition_classes('shortmath');
+        $q = new qtype_shortmath_question();
+        test_question_maker::initialise_a_question($q);
+        $q->name = 'Integration';
+        $q->questiontext = 'Evaluate the integral \( \int _1^2x\sin \left(x^2\right)dx \)';
+        $q->generalfeedback = 'Generalfeedback: \frac{1}{2}\left(\cos(1)-\cos(4)\right) or 0.597 would have been OK.';
+        $q->usecase = false;
+        $q->answers = array(
+            13 => new question_answer(13, '\frac{1}{2}\left(\cos(1)-\cos(4)\right)', 1.0, '\frac{1}{2}\left(\cos(1)-\cos(4)\right) is a very good answer.', FORMAT_HTML),
+            14 => new question_answer(14, '0.597', 0.8, 'Answer in three decimals is an OK approximation.', FORMAT_HTML),
+            15 => new question_answer(15, '*', 0.0, 'That is a bad answer.', FORMAT_HTML),
+        );
+        $q->qtype = question_bank::get_qtype('shortmath');
+
+        return $q;
+    }
+
+    /**
+     * Gets the question data for a shortmath question with with correct
+     * ansewer 'frog', partially correct answer 'toad' and defaultmark 1.
+     * This question also has a '*' match anything answer.
+     * @return stdClass
+     */
+    public function get_shortmath_question_data_integration() {
+        $qdata = new stdClass();
+        test_question_maker::initialise_question_data($qdata);
+
+        $qdata->qtype = 'shortmath';
+        $qdata->name = 'Integration';
+        $qdata->questiontext = 'Evaluate the integral \( \int _1^2x\sin \left(x^2\right)dx \)';
+        $qdata->generalfeedback = 'Generalfeedback: \frac{1}{2}\left(\cos(1)-\cos(4)\right) or 0.597 would have been OK.';
+
+        $qdata->options = new stdClass();
+        $qdata->options->usecase = 0;
+        $qdata->options->answers = array(
+            13 => new question_answer(13, '\frac{1}{2}\left(\cos(1)-\cos(4)\right)', 1.0, '\frac{1}{2}\left(\cos(1)-\cos(4)\right) is a very good answer.', FORMAT_HTML),
+            14 => new question_answer(14, '0.597', 0.8, 'Answer in three decimals is an OK approximation.', FORMAT_HTML),
+            15 => new question_answer(15, '*', 0.0, 'That is a bad answer.', FORMAT_HTML),
+        );
+
+        return $qdata;
+    }
+
+    /**
+     * Gets the question form data for a shortmath question with with correct
+     * answer 'frog', partially correct answer 'toad' and defaultmark 1.
+     * This question also has a '*' match anything answer.
+     * @return stdClass
+     */
+    public function get_shortmath_question_form_data_integration() {
+        $form = new stdClass();
+
+        $form->name = 'Integration';
+        $form->questiontext = array('text' => 'Evaluate the integral \( \int _1^2x\sin \left(x^2\right)dx \)', 'format' => FORMAT_HTML);
+        $form->defaultmark = 1.0;
+        $form->generalfeedback = array('text' => 'Generalfeedback: \frac{1}{2}\left(\cos(1)-\cos(4)\right) or 0.597 would have been OK.', 'format' => FORMAT_HTML);
+        $form->usecase = false;
+        $form->answer = array('integration', 'integration_exact', '*');
+        $form->fraction = array('1.0', '0.8', '0.0');
+        $form->feedback = array(
+            array('text' => '\frac{1}{2}\left(\cos(1)-\cos(4)\right) is a very good answer.', 'format' => FORMAT_HTML),
+            array('text' => 'Answer in three decimals is an OK approximation.', 'format' => FORMAT_HTML),
+            array('text' => 'That is a bad answer.', 'format' => FORMAT_HTML),
+        );
+
+        return $form;
+    }
+
+    /**
+     * Makes a shortmath question with just the correct ansewer 'frog', and
+     * no other answer matching.
+     * @return qtype_shortmath_question
+     */
+    public function make_shortmath_question_integration_exact() {
+        question_bank::load_question_definition_classes('shortmath');
+        $q = new qtype_shortmath_question();
+        test_question_maker::initialise_a_question($q);
+        $q->name = 'Integration exact';
+        $q->questiontext = 'Evaluate the integral \( \int _1^2x\sin \left(x^2\right)dx \)';
+        $q->generalfeedback = 'Generalfeedback: You should have said \frac{1}{2}\left(\cos(1)-\cos(4)\right).';
+        $q->usecase = false;
+        $q->answers = array(
+            13 => new question_answer(13, '\frac{1}{2}\left(\cos(1)-\cos(4)\right)', 1.0, '\frac{1}{2}\left(\cos(1)-\cos(4)\right) is correct.', FORMAT_HTML),
+        );
+        $q->qtype = question_bank::get_qtype('shortmath');
+
+        return $q;
+    }
+
+    /**
+     * Gets the question data for a shortmath questionwith just the correct
+     * ansewer 'frog', and no other answer matching.
+     * @return stdClass
+     */
+    public function get_shortmath_question_data_integration_exact() {
+        $qdata = new stdClass();
+        test_question_maker::initialise_question_data($qdata);
+
+        $qdata->qtype = 'shortmath';
+        $qdata->name = 'Integration exact';
+        $qdata->questiontext = 'Evaluate the integral \( \int _1^2x\sin \left(x^2\right)dx \)';
+        $qdata->generalfeedback = 'Generalfeedback: You should have said \frac{1}{2}\left(\cos(1)-\cos(4)\right).';
+
+        $qdata->options = new stdClass();
+        $qdata->options->usecase = false;
+        $qdata->options->answers = array(
+            13 => new question_answer(13, '\frac{1}{2}\left(\cos(1)-\cos(4)\right)', 1.0, '\frac{1}{2}\left(\cos(1)-\cos(4)\right) is correct.', FORMAT_HTML),
+        );
+
+        return $qdata;
+    }
+
+    /**
+     * Makes a shortmath question with just the correct ansewer 'frog', and
+     * no other answer matching.
+     * @return qtype_shortmath_question
+     */
+    public function make_shortmath_question_escapedwildcards() {
+        question_bank::load_question_definition_classes('shortmath');
+        $q = new qtype_shortmath_question();
+        test_question_maker::initialise_a_question($q);
+        $q->name = 'Question with escaped * in the answer.';
+        $q->questiontext = 'How to you write x times y in C? __________';
+        $q->generalfeedback = 'In C, this expression is written x * y.';
+        $q->usecase = false;
+        $q->answers = array(
+            13 => new question_answer(13, '*x\*y*', 1.0, 'Well done.', FORMAT_HTML),
+        );
+        $q->qtype = question_bank::get_qtype('shortmath');
+
+        return $q;
+    }
+}
---
+++ tests/questiontype_test.php
@@ -0,0 +1,164 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Unit tests for the ShortMath question type class.
+ *
+ * @package    qtype_shortmath
+ * @author     André Storhaug <andr3.storhaug@gmail.com>
+ * @copyright  2018 NTNU
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+defined('MOODLE_INTERNAL') || die();
+
+global $CFG;
+require_once($CFG->dirroot . '/question/type/shortmath/questiontype.php');
+require_once($CFG->dirroot . '/question/engine/tests/helpers.php');
+require_once($CFG->dirroot . '/question/type/edit_question_form.php');
+require_once($CFG->dirroot . '/question/type/shortmath/edit_shortmath_form.php');
+
+/**
+ * Unit tests for the ShortMath question type class.
+ *
+ * @author     André Storhaug <andr3.storhaug@gmail.com>
+ * @copyright  2018 NTNU
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class qtype_shortmath_test extends advanced_testcase {
+    public static $includecoverage = array(
+        'question/type/questiontypebase.php',
+        'question/type/shortmath/questiontype.php',
+    );
+
+    protected $qtype;
+
+    protected function setUp() {
+        $this->qtype = new qtype_shortmath();
+    }
+
+    protected function tearDown() {
+        $this->qtype = null;
+    }
+
+    protected function get_test_question_data() {
+        return test_question_maker::get_question_data('shortmath');
+    }
+
+    public function test_name() {
+        $this->assertEquals($this->qtype->name(), 'shortmath');
+    }
+
+    public function test_can_analyse_responses() {
+        $this->assertTrue($this->qtype->can_analyse_responses());
+    }
+
+    public function test_get_random_guess_score() {
+        $q = test_question_maker::get_question_data('shortmath');
+        $q->options->answers[15]->fraction = 0.1;
+        $this->assertEquals(0.1, $this->qtype->get_random_guess_score($q));
+    }
+
+    public function test_get_possible_responses() {
+        $q = test_question_maker::get_question_data('shortmath');
+
+        $this->assertEquals(array(
+            $q->id => array(
+                13 => new question_possible_response('\frac{1}{2}\left(\cos(1)-\cos(4)\right)', 1),
+                14 => new question_possible_response('0.597', 0.8),
+                15 => new question_possible_response('*', 0),
+                null => question_possible_response::no_response()
+            ),
+        ), $this->qtype->get_possible_responses($q));
+    }
+
+    public function test_get_possible_responses_no_star() {
+        $q = test_question_maker::get_question_data('shortmath', 'integration_exact');
+
+        $this->assertEquals(array(
+            $q->id => array(
+                13 => new question_possible_response('\frac{1}{2}\left(\cos(1)-\cos(4)\right)', 1),
+                0 => new question_possible_response(get_string('didnotmatchanyanswer', 'question'), 0),
+                null => question_possible_response::no_response()
+            ),
+        ), $this->qtype->get_possible_responses($q));
+    }
+
+    public function test_question_saving_integration_exact() {
+        $this->resetAfterTest(true);
+        $this->setAdminUser();
+
+        $questiondata = test_question_maker::get_question_data('shortmath');
+        $formdata = test_question_maker::get_question_form_data('shortmath');
+
+        $generator = $this->getDataGenerator()->get_plugin_generator('core_question');
+        $cat = $generator->create_question_category(array());
+
+        $formdata->category = "{$cat->id},{$cat->contextid}";
+        qtype_shortmath_edit_form::mock_submit((array)$formdata);
+
+        $form = qtype_shortmath_test_helper::get_question_editing_form($cat, $questiondata);
+
+        $this->assertTrue($form->is_validated());
+
+        $fromform = $form->get_data();
+
+        $returnedfromsave = $this->qtype->save_question($questiondata, $fromform);
+        $actualquestionsdata = question_load_questions(array($returnedfromsave->id));
+        $actualquestiondata = end($actualquestionsdata);
+
+        foreach ($questiondata as $property => $value) {
+            if (!in_array($property, array('id', 'version', 'timemodified', 'timecreated', 'options'))) {
+                $this->assertAttributeEquals($value, $property, $actualquestiondata);
+            }
+        }
+
+        foreach ($questiondata->options as $optionname => $value) {
+            if ($optionname != 'answers') {
+                $this->assertAttributeEquals($value, $optionname, $actualquestiondata->options);
+            }
+        }
+    }
+
+    public function test_question_saving_trims_answers() {
+        $this->resetAfterTest(true);
+        $this->setAdminUser();
+
+        $questiondata = test_question_maker::get_question_data('shortmath');
+        $formdata = test_question_maker::get_question_form_data('shortmath');
+
+        $generator = $this->getDataGenerator()->get_plugin_generator('core_question');
+        $cat = $generator->create_question_category(array());
+
+        $formdata->category = "{$cat->id},{$cat->contextid}";
+        $formdata->answer[0] = '   \frac{1}{2}\left(\cos(1)-\cos(4)\right)   ';
+        qtype_shortmath_edit_form::mock_submit((array)$formdata);
+
+        $form = qtype_shortmath_test_helper::get_question_editing_form($cat, $questiondata);
+
+        $this->assertTrue($form->is_validated());
+
+        $fromform = $form->get_data();
+
+        $returnedfromsave = $this->qtype->save_question($questiondata, $fromform);
+        $actualquestionsdata = question_load_questions(array($returnedfromsave->id));
+        $actualquestiondata = end($actualquestionsdata);
+
+        $firstsavedanswer = reset($questiondata->options->answers);
+        $this->assertEquals('\frac{1}{2}\left(\cos(1)-\cos(4)\right)', $firstsavedanswer->answer);
+    }
+}
