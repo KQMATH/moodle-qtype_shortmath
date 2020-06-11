a04a20a75c7f22ef2862d821d341f29560d267c0
sushantk-stud <55709595+sushantk-stud@users.noreply.github.com>
2020-06-11 18:52:49 +0200

    #32 Configurable Toolbar

--- amd/src/view-editor.js
+++ amd/src/view-editor.js
@@ -24,9 +24,129 @@
  * @module qtype_shortmath/input
  */
 define(['jquery', 'qtype_shortmath/visual-math-input'], function($, VisualMath) {
-    return {
+    /*return {
         initialize: function(inputname, readonly) {
-            console.log("ADD CONTENTS HERE!!")
+            console.log("ADD CONTENTS HERE!!");
+        }
+    };*/
+    class EditorInput extends VisualMath.Input {
+        change(input) {
+            input.onEdit = ($input, field) => {
+                console.log("changed ");
+                $input.val(field.latex());
+                $input.get(0).dispatchEvent(new Event('change')); // Event firing needs to be on a vanilla dom object.
+            };
+        }
+
+        save($btnInput, $expInput, controlsWrapper) {
+            let btnText = $btnInput.val().replace(/[{}\s]/g, '');
+            let expText = $expInput.val().replace(/[{}\s]/g, '');
+            console.log(btnText);
+            console.log(expText);
+            let control;
+            let caret = '';
+            let chars;
+            if(expText.replace(/[{}a-z0-9A-Z\s]/g, '') === '^'){
+                let chars = btnText.split('^');
+                console.log(chars[0]+' '+chars[1]);
+                caret += '<div class="mq-math-mode" style="cursor:pointer;font-size:100%;">';
+                caret += '<span class="mq-root-block">';
+                caret += '<var>${chars[0]}</var>';
+                caret += '<span class="mq-supsub mq-non-leaf mq-sup-only">';
+                caret += '<span class="mq-sup">';
+                caret += '<var>${chars[1]}</var>';
+                caret += '</span></span></span></div>';
+            }
+            else if(expText.includes('lim_')){
+                let lim = '<span class="mq-root-block">lim</span>';
+                console.log('lim');
+                let chars = expText.split('\\');
+                console.log(chars);
+                control = new VisualMath.Control('lim', lim, field => {
+                    field.cmd('\\lim').typedText('_').write(chars[1].split('_')[1])
+                        .cmd('\\to').write(expText.split('to')[1]).moveToRightEnd();
+                });
+            }
+            /*control = new VisualMath.Control('caret', caret, field => field.cmd('^'));*/
+            control.enable();
+            controlsWrapper.append(control.$element);
+
+        }
+    }
+
+    return {
+        initialize: function(inputName, readonly, btnName, expName) {
+            console.log("name:  "+inputName);
+            console.log("ro:  "+readonly);
+            var readOnly = readonly;
+            var $shortanswerInput = $('#' + $.escapeSelector(inputName));
+            // Remove class "d-inline" added in shortanswer renderer class, which prevents input from being hidden.
+            $shortanswerInput.removeClass('d-inline');
+            var $parent = $('#' + $.escapeSelector(inputName)).parent('.answer');
+
+            var input = new VisualMath.Input($shortanswerInput, $parent);
+            console.log("test value:  "+input.value);
+            input.$input.hide();
+
+            let editorInput = new EditorInput();
+            if (!readonly) {
+                /*input.onEdit = function($input, field) {
+                    console.log("changed ");
+                    $input.val(field.latex());
+                    $input.get(0).dispatchEvent(new Event('change')); // Event firing needs to be on a vanilla dom object.
+                };*/
+
+                editorInput.change(input);
+
+            } else {
+                readOnly = true;
+                input.disable();
+            }
+
+            if ($shortanswerInput.val()) {
+                input.field.write(
+                    $shortanswerInput.val()
+                );
+            }
+
+            // if (!readOnly) {
+                var controlsWrapper = $('#' + $.escapeSelector(inputName)).parents('.shortmath').find('.controls_wrapper');
+                var controls = new VisualMath.ControlList(controlsWrapper);
+                controls.enableAll();
+            // }
+            // let controlsWrapper = $('#' + $.escapeSelector(inputName)).parents('.shortmath').find('.controls_wrapper');
+
+            let $btnInput = $('#' + $.escapeSelector(btnName));
+            // Remove class "d-inline" added in shortanswer renderer class, which prevents input from being hidden.
+            $btnInput.removeClass('d-inline');
+            $parent = $('#' + $.escapeSelector(btnName)).parent('.answer');
+
+            input = new VisualMath.Input($btnInput, $parent);
+            console.log("test value:  "+input.value);
+            input.$input.hide();
+            editorInput.change(input);
+
+            let $expInput = $('#' + $.escapeSelector(expName));
+            // Remove class "d-inline" added in shortanswer renderer class, which prevents input from being hidden.
+            $expInput.removeClass('d-inline');
+            $parent = $('#' + $.escapeSelector(expName)).parent('.answer');
+
+            input = new VisualMath.Input($expInput, $parent);
+            console.log("test value:  "+input.value);
+            input.$input.hide();
+            editorInput.change(input);
+
+            let $saveButton = $('#' + $.escapeSelector('save'));
+            $saveButton.on('click', event => {
+                event.preventDefault();
+                /*if (lastFocusedInput !== null) {
+                    this.onClick(lastFocusedInput.field);
+                    lastFocusedInput.field.focus();
+                }*/
+                console.log("save");
+                //console.log($expInput.val().replace(/[{}a-z0-9A-Z\s]/g, ''));
+                editorInput.save($btnInput, $expInput, controlsWrapper);
+            });
         }
     };
 });
--- view_editor.php
+++ view_editor.php
@@ -32,17 +32,62 @@ $title = "Editor configuration";
 $PAGE->set_title($title);
 $PAGE->set_pagelayout('standard');
 
+$PAGE->requires->css('/question/type/shortmath/visualmathinput/mathquill.css');
+$PAGE->requires->css('/question/type/shortmath/visualmathinput/visual-math-input.css');
 
 echo $OUTPUT->header();
 echo $OUTPUT->heading($title);
 
 
 // render page
-echo html_writer::tag('div', "Edit the file under moodle/question/type/shortmath/amd/src/view-edit.js");
+//echo html_writer::tag('div', "Edit the file under moodle/question/type/shortmath/amd/src/view-edit.js");
 
+//echo html_writer::tag('input', "", array('type' => 'text', 'id' => 'smath'));
+
+$inputattributes = array(
+    'type' => 'text',
+    'name' => 'test',
+//    'value' => '',
+    'id' => 'test',
+    'size' => 30,
+    //'class' => 'form-control d-inline'
+);
+
+//$input = html_writer::empty_tag('input', array('id' => 'test'));
+$input = html_writer::empty_tag('input', $inputattributes);
+
+// Test
+echo html_writer::start_tag('div', array('class' => 'que shortmath'));
+//echo html_writer::tag('div', '', array('class' => 'info'));
+//echo html_writer::start_tag('div', array('class' => 'content'));
+echo html_writer::tag('div', '', array('class' => 'controls_wrapper'));
+//echo html_writer::tag('div', '', array('class' => 'qtext'));
+echo html_writer::start_tag('div', array('class' => 'ablock form-inline'));
+//echo html_writer::tag('label', get_string('answercolon', 'qtype_numerical'), array('for' => 'test'));
+echo html_writer::tag('span', $input, array('class' => 'answer'));
+echo html_writer::end_tag('div');
+//echo html_writer::end_tag('div');
+//echo html_writer::end_tag('div');
+
+
+echo html_writer::start_tag('div', array('class' => 'ablock form-inline'));
+echo html_writer::tag('label', 'Button:', array('for' => 'btn'));
+echo html_writer::tag('span', html_writer::empty_tag('input',
+    array('type' => 'text', 'name' => 'btn', 'id' => 'btn', 'size' => 30)),
+    array('class' => 'answer'));
+echo html_writer::tag('label', 'Expression:', array('for' => 'exp'));
+echo html_writer::tag('span', html_writer::empty_tag('input',
+    array('type' => 'text', 'name' => 'exp', 'id' => 'exp', 'size' => 30)),
+    array('class' => 'answer'));
+echo html_writer::end_tag('div');
+
+echo html_writer::start_tag('div', array());
+echo html_writer::tag('input', '', array('type' => 'button', 'name' => 'save',
+    'id' => 'save', 'class' => 'btn btn-primary', 'value' => 'Save'));
+echo html_writer::end_tag('div');
 
 // Execute js script
-$params = []; // JS params passed here...
+$params = ['test', false, 'btn', 'exp']; // JS params passed here...
 $PAGE->requires->js_call_amd('qtype_shortmath/view-editor', 'initialize', $params);
 
 echo $OUTPUT->footer();
