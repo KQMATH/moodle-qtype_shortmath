8c290909de5fa80a724b59a0251e24d67b632918
André Storhaug <andr3.storhaug@gmail.com>
2021-02-03 00:23:46 +0100

    Fixes #33 - backup and restore issues

--- backup/moodle2/backup_qtype_shortmath_plugin.class.php
+++ backup/moodle2/backup_qtype_shortmath_plugin.class.php
@@ -39,7 +39,7 @@ class backup_qtype_shortmath_plugin extends backup_qtype_plugin {
      */
     protected function define_question_plugin_structure() {
         // Define the virtual plugin element with the condition to fulfill.
-        $plugin = $this->get_plugin_element(null, '../../qtype', 'shortmath');
+        $plugin = $this->get_plugin_element(null, '../../qtype', $this->pluginname);
         // Create one standard named plugin element (the visible container).
         $pluginwrapper = new backup_nested_element($this->get_recommended_name());
         // Connect the visible container ASAP.
@@ -48,7 +48,7 @@ class backup_qtype_shortmath_plugin extends backup_qtype_plugin {
         // to the tree before any other information that will use them.
         $this->add_question_question_answers($pluginwrapper);
         // Now create the qtype own structures.
-        $shortmath = new backup_nested_element('shortmath', ['id'], ['answers', 'usecase', 'usehint']);
+        $shortmath = new backup_nested_element('shortmath', ['id'], ['questionid', 'usecase']);
         // Now the own qtype tree.
         $pluginwrapper->add_child($shortmath);
         // Set source to populate the data.
